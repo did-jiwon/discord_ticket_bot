59e29830fa9132bc02157cb4e7d38fae60c3ea74
did-jiwon <puding0712@naver.com>
2023-04-23 18:12:45 +0900

    rm 로그인 페이지

--- 로그인 페이지/check-db.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once 'db.class.php';
-
-    $result = DB::query('SELECT * FROM topic');
-    $string = json_encode($result);
-    echo "$string";
-?>
--- 로그인 페이지/list-delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $tmp_id = $_POST['id'];
-    $conn = mysqli_connect("localhost", "root", "111111", "opentutorials");
-    $sql = "DELETE FROM jjim WHERE id=$tmp_id";
-    mysqli_query($conn, $sql);
-    //echo mysqli_error($conn);
-    print "<script language=javascript> alert('리스트에서 제거됨'); history.back(-2); </script>";
-    mysqli_close($conn);
-?>
